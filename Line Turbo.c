856e31f35130fcc82a86ede7b3661d8452d43fea
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2025-11-01 22:02:27 +0530

    Create Line Turbo.c

---
+++ Line Turbo.c
@@ -0,0 +1,14 @@
+//
+
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+
+void main(){
+	int gDriver = DETECT , gmode ;
+	initgraph(&gDriver , &gmode , "C:\\TURBOC3\\BGI");
+	setcolor(GREEN);
+	line(100,200,900,100);
+	getch();
+
+}
